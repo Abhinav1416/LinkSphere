772086aad183a1baa0bc3b81aa227bd215c56eef
Abhinav1416 <gudipudiabhinav2005@gmail.com>
2025-05-29 00:54:58 +0530

    Update build.gradle.kts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -40,9 +40,6 @@ dependencies {
     // DevTools
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    // Lombok
-    compileOnly("org.projectlombok:lombok:1.18.28")
-    annotationProcessor("org.projectlombok:lombok:1.18.28")
 
     // Testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
